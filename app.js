c20febfbe7cb985b8fda99f6ae5c7269fb39ff15
Tyler Lawson <tylerlawson12@live.com>
2019-10-01 14:30:04 -0700

    final outcome to assignment

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var likesDirtBiking = prompt('Do you like dirt biking?');
 
 likesDirtBiking = likesDirtBiking.toUpperCase();
 
-if(likesDirtBiking === 'YES') {
+if (likesDirtBiking === 'YES') {
   alert('Cool so do I.');
 } else {
   alert('That is okay');
@@ -12,8 +12,8 @@ var likesLearningNewThings = prompt('Do you like learning new things?');
 
 likesLearningNewThings = likesLearningNewThings.toUpperCase();
 
-if(likesLearningNewThings === 'YES') {
-  alert('That\'s awesome so do I');
+if (likesLearningNewThings === 'YES') {
+  alert('That\'s awesome so do I, learning gives us the ability to make a difference in a world thank is constantly changing');
 } else {
   alert('That\'s okay it\'s not meant for everybody.');
 }
@@ -24,8 +24,8 @@ var likesTraveling = prompt('Do you like to travel?');
 
 likesTraveling = likesTraveling.toUpperCase();
 
-if(likesTraveling === 'YES') {
-  alert('Traveling is an amzing experience and I\'m glad you have had the chance.');
+if (likesTraveling === 'YES') {
+  alert('Traveling is an amazing experience and I\'m glad you have had the chance.');
 } else {
   alert('Traveling can be pricey and time consuming.');
 }
@@ -36,10 +36,21 @@ var likesDogs = prompt('Do you like dogs?');
 
 likesDogs = likesDogs.toUpperCase();
 
-if(likesDogs === 'YES') {
+if (likesDogs === 'YES') {
   alert('Dogs are amazing good choice.');
 } else {
   alert('There always has to be one cat person lol.');
 }
 
 
+var WearsGlasses = prompt('Do I wear glasses?');
+
+WearsGlasses = WearsGlasses.toUpperCase();
+
+if (WearsGlasses === 'YES') {
+  alert('You\'re right');
+} else {
+  alert('You\'re wrong!');
+}
+
+alert('Thank you for answering my questions, enjoy my about me.');
