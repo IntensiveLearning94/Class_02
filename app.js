36a49feb07ead3cd041429dc2341740ff5505d91
Tyler Lawson <tylerlawson12@live.com>
2019-10-01 13:18:57 -0700

    2nd question for about me

--- app.js
+++ app.js
@@ -1,2 +1,22 @@
-console.log('I am alive');
+var likesDirtBiking = prompt('Do you like dirt biking?');
+
+likesDirtBiking = likesDirtBiking.toUpperCase();
+
+if(likesDirtBiking === 'YES') {
+  alert('Cool so do I.');
+} else {
+  alert('That is okay');
+}
+
+var likesLearningNewThings = prompt('Do you like learning new things?');
+
+likesLearningNewThings = likesLearningNewThings.toUpperCase();
+
+if(likesLearningNewThings === 'YES') {
+  alert('That\'s awesome so do I');
+} else {
+  alert('That\'s okay it\'s not meant for everybody.');
+}
+
+
 
